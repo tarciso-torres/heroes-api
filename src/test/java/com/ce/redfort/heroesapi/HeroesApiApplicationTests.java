d8e6530f8e2f58accb83436d1ecb49e0e97520a5
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-13 13:29:16 -0300

    chore: Criação do projeto

---
+++ src/test/java/com/ce/redfort/heroesapi/HeroesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ce.redfort.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
